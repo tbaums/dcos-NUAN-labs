6ec60681c811851a0c888da8b7114269e8b0b664
tbaums <michael.a.tanenbaum@gmail.com>
2019-06-14 09:47:59 -0400

    update main.tf

--- clusters/setup/main.tf
+++ clusters/setup/main.tf
@@ -4,7 +4,7 @@ data "http" "whatismyip" {
 }
 
 provider "aws" {
- region = "us-east-1"
+ region = "us-west-2"
 }
 
 module "dcos" {
@@ -13,33 +13,30 @@ module "dcos" {
   subnet_range = "172.16.0.0/16"
 
   dcos_instance_os    = "centos_7.5"
-  cluster_name        = "nuan-workshop-07"
+  cluster_name        = "nuan-workshop-01"
   ssh_public_key_file = "workshop.id_rsa.pub"
   admin_ips           = ["0.0.0.0/0"]
 
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
   num_masters        = "1"
   num_private_agents = "6"
   num_public_agents  = "1"
 
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   bootstrap_instance_type = "t2.medium"
   public_agents_instance_type = "m5.xlarge"
   private_agents_instance_type = "c5.4xlarge"
   masters_instance_type = "m5.xlarge"
-  dcos_version = "1.12.1"
-  dcos_resolvers      = "\n   - 169.254.169.253"
+  dcos_version = "1.13.1"
+ #  dcos_resolvers      = "\n   - 169.254.169.253"
 
   # dcos_variant = "open"
   dcos_variant = "ee"
-  dcos_license_key_contents = "${file("license.txt")}"
+  dcos_license_key_contents = "${file("~/scripts/license.txt")}"
 
-  tags = {workshop = "Nuance"} 
+  tags = {owner = "mtanenbaum", expiration = "4h"} 
 
   public_agents_additional_ports = ["6090", "6443", "7443", "3000", "9090", "9093", "9091", "30001", "30443"]
 
-  dcos_superuser_username = "admin"
-
-  # dcos_superuser_password_hash = "$6$rounds=656000$jlw1TpTER3CnUdeM$DIV6NZNFcLFSlZ5JlxLGgPNKBEvKdPO/KUq5KsItn/mKGBWRzhsbJNQKwlHElSgHAJ.cOJZWmlOtC2TWJuBDI."
 }
 
 output "masters-ips" {
